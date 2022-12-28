761e50b9496b22b67fc6aaf98db3404b1684d1b8
Niclas Overby <niclas@overby.me>
2022-12-28 19:07:26 +0100

    Small cleanup

--- src/anilist.js
+++ src/anilist.js
@@ -1,44 +1,37 @@
-let drawTable = (list_to_draw) => {
+const drawTable = (listToDraw) => {
     const list = document.getElementById("list");
     const header = document.getElementById("list-header");
-    let list_content = header.outerHTML;
-
-    for (const entry of list_to_draw) {
-        list_content += `<tr>
+    const string = listToDraw
+        .map(
+            (entry) => `<tr>
                 <td><a href="${entry.link}">${entry.name}</a></td>
                 <td>${entry.status}</td>
                 <td>${entry.episodes}</td>
                 <td>${entry.watched}</td>
-                <td>${entry.last_updated}</td>
-            </tr>`;
-    }
+                <td>${entry.lastUpdated}</td>
+            </tr>`
+        )
+        .join("");
 
-    list.innerHTML = list_content;
+    list.innerHTML = header.outerHTML + string;
 };
 
-let animelist = [];
 fetch(
-    "https://raw.githubusercontent.com/Hejsil/dotfiles/master/local/share/aniz/list",
+    "https://raw.githubusercontent.com/Hejsil/dotfiles/master/local/share/aniz/list"
 )
     .then((reponse) => reponse.text())
     .then((text) => {
-        let lines = text.split("\n");
-        for (const line of lines) {
+        const lines = text.trim().split("\n");
+        const animelist = lines.map((line) => {
             const entries = line.split("\t");
-            const entry = {
-                last_updated: entries[0],
+            return {
+                lastUpdated: entries[0],
                 status: entries[1],
                 episodes: entries[2],
                 watched: entries[3],
                 name: entries[4],
                 link: entries[5],
             };
-            if (entry.link === undefined) {
-                break;
-            }
-
-            animelist.push(entry);
-        }
-
+        });
         drawTable(animelist);
     });
