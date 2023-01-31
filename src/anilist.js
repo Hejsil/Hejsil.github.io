5aa9726c6dbf719e0a6f124becb4db72e46ceca1
Jimmi Holst Christensen <jhc@dismail.de>
2023-01-31 19:17:17 +0100

    anilist: Give status meaningful names

--- src/anilist.js
+++ src/anilist.js
@@ -1,23 +1,47 @@
 const drawTable = (listToDraw) => {
     const list = document.getElementById("list");
-    const header = document.getElementById("list-header");
-    const string = listToDraw
-        .map(
-            (entry) => `<tr>
+    const header = `<tr>
+        <th>Anime</th>
+        <th>Status</th>
+        <th>Episodes Watched</th>
+        <th>Watch Count</th>
+        <th>Last Updated</th>
+    </tr>`;
+
+    const table = listToDraw
+        .map((entry) => {
+            const status = (() => {
+                switch (entry.status) {
+                    case "d":
+                        return "Dropped";
+                    case "c":
+                        return "Completed";
+                    case "w":
+                        return "Watching";
+                    case "o":
+                        return "On hold";
+                    case "p":
+                        return "Planning to watch";
+                    default:
+                        return "???";
+                }
+            })();
+
+            return `<tr>
                 <td><a href="${entry.link}">${entry.name}</a></td>
-                <td>${entry.status}</td>
+                <td>${status}</td>
                 <td>${entry.episodes}</td>
                 <td>${entry.watched}</td>
                 <td>${entry.lastUpdated}</td>
-            </tr>`
-        )
+            </tr>`;
+        })
         .join("");
 
-    list.innerHTML = header.outerHTML + string;
+    list.innerHTML = header + table;
 };
 
 fetch(
-    "https://raw.githubusercontent.com/Hejsil/dotfiles/master/local/share/aniz/list"
+    "https://raw.githubusercontent.com/Hejsil/dotfiles/master/local/share/aniz/list",
 )
     .then((reponse) => reponse.text())
     .then((text) => {
